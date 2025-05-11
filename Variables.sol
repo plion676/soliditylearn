755cedc8184317f178927ebb23abe14a75b17f7c
plion676 <plion676@outlook.com>
2025-05-11 18:51:44 +0800

    Update Variables.sol

--- Variables.sol
+++ Variables.sol
@@ -9,7 +9,7 @@ contract Variables {
     function updateNumber(uint newnumber) public{
         if(newnumber>0) {
             number = newnumber;
-        }
+        }//更改number，函数内部变量无需memory位置定义，因为uint(byte32,address,boll,int)是直接存储具体值的值类型，string等引用类型才需要
     }
 
     function getNumber() public view returns (uint) {
