c25057299d389daa9de1d5a5e5d6cf78345dab32
plion676 <plion676@outlook.com>
2025-05-19 21:46:23 +0800

    new contract

---
+++ contracts/MiniAMM.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+contract MiniAMM {
+
+    uint public xReserve;
+    uint public yReserve;
+
+    function init(uint X,uint Y) public{
+        xReserve=X;
+        yReserve=Y;
+    }
+    function swapXforY(uint amount)public view returns(uint dy){
+        uint newX=xReserve+amount;
+        uint newY=xReserve*yReserve/newX;
+        return yReserve-newY;
+    }
+    
+}
