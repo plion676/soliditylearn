bd46bd53b864026f73b67fc0d9ee72d0a5fbab97
plion676 <plion676@outlook.com>
2025-05-27 01:50:34 +0800

    update

--- contracts/MyERC20WithPermit.sol
+++ contracts/MyERC20WithPermit.sol
@@ -86,13 +86,3 @@ contract MyERC20WithPermit is ERC20{
 }
 
 }
-
-
-
-DOMAIN_SEPARATOR=keccak256(
-            abi.encode(
-                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
-                keccak256(bytes(name)),
-                keccak256(bytes("1")),
-                chainId,
-                address(this)
